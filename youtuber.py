20b24306abfa35a225021e5f7b60059c8df53386
adarsh2858 <44858222+adarsh2858@users.noreply.github.com>
2019-07-02 17:08:37 +0530

    alternate method

--- youtuber.py
+++ youtuber.py
@@ -1,40 +1,19 @@
 import youtube_dl
-
-ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s', 'quiet':True,})
-video = ""
-
-with ydl:
-    yt_url=input()
-    result = ydl.extract_info(yt_url, download=False) #We just want to extract the info
-
-    if 'entries' in result:
-        # Can be a playlist or a list of videos
-        video = result['entries']
-
-        print('| Topic        |      Video     |  Presentation |')
-        print('| ------------- |:-------------:| -----:|')
-
-        #loops entries to grab each video_url
-        for i, item in enumerate(video):
-            video = result['entries'][i]
-            # fetch the url and title of video
-            print('|'+ result['entries'][i]['title'] +'|[Watch Here]('+result['entries'][i]['webpage_url']+')|'+'-|')
-            
-#Alternate method
-import youtube_dl
 import os
 
-input=' '+input()
+# input=' https://www.youtube.com/playlist?list=PLj6h78yzYM2PpmMAnvpvsnR4c27wJePh3'
+url=' '+input()
 start = 1
-end = input('Number of videos:')
+print("Enter number of videos:")
+end = int(input())
 
 # For fetching the title of the video in playlist
-cmd_code1='py -m youtube_dl -e --playlist-start ' +str(start)+' --playlist-end '+str(end)+input
+cmd_code1='py -m youtube_dl -e --playlist-start ' +str(start)+' --playlist-end '+str(end)+url
 title = os.popen(cmd_code1).read().splitlines()
 #print(title[0])
 
 # For fetching the id of that particular video in playlist
-cmd_code2='py -m youtube_dl --get-id --playlist-start ' +str(start)+' --playlist-end '+str(end)+input
+cmd_code2='py -m youtube_dl --get-id --playlist-start ' +str(start)+' --playlist-end '+str(end)+url
 video_id = os.popen(cmd_code2).read().splitlines()
 
 print('| Topic        |      Video     |  Presentation |')
